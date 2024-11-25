dc9c9a16c3dd8642f9fa6c8dd38adb76ff8eab61
mAmineChniti <emin.chniti@esprit.tn>
2024-11-25 11:33:17 +0100

    Prosit 10

--- src/IDepartement.java
+++ src/IDepartement.java
@@ -1,2 +1,10 @@
-public interface IDepartement {
+import java.util.TreeSet;
+
+public interface IDepartement<T> {
+    void ajouterDepartement(T t);
+    boolean rechercherDepartement(String nom);
+    boolean rechercherDepartement(T t);
+    void supprimerDepartement(T t);
+    void displayDepartement();
+    TreeSet<T> trierDepartementById();
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        DepartementHashSet gestion = new DepartementHashSet();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Departement d1 = new Departement(1, "Informatique", 10);
+        Departement d2 = new Departement(2, "Ressources Humaines", 5);
+        Departement d3 = new Departement(3, "Finance", 8);
+
+        gestion.ajouterDepartement(d1);
+        gestion.ajouterDepartement(d2);
+        gestion.ajouterDepartement(d3);
+
+        System.out.println("\nRecherche par nom 'Informatique': " + gestion.rechercherDepartement("Informatique"));
+        System.out.println("Recherche par objet 'd1': " + gestion.rechercherDepartement(d1));
+
+        System.out.println("\nAffichage des départements :");
+        gestion.displayDepartement();
+
+        System.out.println("\nSuppression du département 'd2':");
+        gestion.supprimerDepartement(d2);
+
+        System.out.println("\nAffichage après suppression :");
+        gestion.displayDepartement();
+
+        System.out.println("\nDépartements triés par ID :");
+        for (Departement dep : gestion.trierDepartementById()) {
+            System.out.println(dep);
         }
     }
 }
